5fb1748e7e871341f4d604a8badb38c4e23ad88c
Adrian Tubio Noya <adrian.tubio.noya@oracle.com>
2022-07-04 16:43:04 +0100

    Fix indent scope

--- lua/config/indent-blankline.lua
+++ lua/config/indent-blankline.lua
@@ -7,11 +7,12 @@ require("indent_blankline").setup {
     show_trailing_blankline_indent = false,
     show_current_context = true,
     show_current_context_start = true,
+    use_treesitter_scope = true,
     filetype_exclude = {
-        "startify", "dashboard", "dotooagenda", "log", "fugitive", "gitcommit",
-        "packer", "vimwiki", "txt", "vista", "help",
-        "todoist", "NvimTree", "peekaboo", "git", "TelescopePrompt", "undotree",
-        "flutterToolsOutline", "" -- for all buffers without a file type
+      "startify", "dashboard", "dotooagenda", "log", "fugitive", "gitcommit",
+      "packer", "vimwiki", "txt", "vista", "help",
+      "todoist", "NvimTree", "peekaboo", "git", "TelescopePrompt", "undotree",
+      "flutterToolsOutline", "" -- for all buffers without a file type
     },
     buftype_exclude = {"terminal", "nofile"},
     context_pattern_highlight = "IndentBlanklineContextPattern"
